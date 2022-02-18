3e45f9207238555b2f90aaa2b3777a2b4402a906
Gilles <gitkraken@flisch.be>
2022-02-18 18:09:51 +0100

    Check build issue

---
+++ src/Arc4u.Standard.OAuth.AspNetCore.Msal/TokenProvider/Obo/OboTokenProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arc4u.OAuth2.Msal.TokenProvider.Obo
+{
+    internal class OboTokenProvider
+    {
+    }
+}
