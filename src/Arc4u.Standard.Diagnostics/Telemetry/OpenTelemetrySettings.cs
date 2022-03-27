018ac86805461be26bd69d292b0d5f5868193e7e
Gilles Flisch <info@arc4u.net>
2022-03-27 08:30:06 +0200

    Add OpenTelemetry configuration

---
+++ src/Arc4u.Standard.Diagnostics/Telemetry/OpenTelemetrySettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Arc4u.Diagnostics
+{
+    public class OpenTelemetrySettings
+    {
+        public string Address { get; set; }
+
+        public Dictionary<string, object> Attributes { get; set; }
+
+        public List<string> Sources { get; set; }
+    }
+}
+}
