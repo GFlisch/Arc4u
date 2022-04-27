32e5c82c1c7c622eeae4466bbc09167bad73b925
Gilles Flisch <info@arc4u.net>
2022-04-27 16:18:17 +0200

    Improve logging

--- src/Arc4u.Standard.Diagnostics/Fluent/Common/CommonMessageLogger.cs
+++ src/Arc4u.Standard.Diagnostics/Fluent/Common/CommonMessageLogger.cs
@@ -12,38 +12,70 @@ namespace Arc4u.Diagnostics
             MessageLogger = new LoggerMessage(logger, category, methodName, typeClass);
         }
 
-        private CommonLoggerProperties AddEntry(LogLevel logLevel, string message, Exception exception = null)
+        private CommonLoggerProperties AddEntry(LogLevel logLevel, string message, Exception exception = null, object[] args = null)
         {
             MessageLogger.LogLevel = logLevel;
             MessageLogger.Text = message;
+            MessageLogger.Args = args;
             MessageLogger.Exception = exception;
+
 
             return new CommonLoggerProperties(MessageLogger);
         }
 
-        public CommonLoggerProperties Debug(string message)
+        public CommonLoggerProperties Debug(string message, params object[] args)
         {
-            return AddEntry(LogLevel.Debug, message);
+            return AddEntry(LogLevel.Debug, message, null, args);
         }
 
-        public CommonLoggerProperties Information(string message)
+        public void LogDebug(string message, params object[] args)
         {
-            return AddEntry(LogLevel.Information, message);
+            var buildMessage = AddEntry(LogLevel.Debug, message, null, args);
+            buildMessage.Log();
         }
 
-        public CommonLoggerProperties Warning(string message)
+        public CommonLoggerProperties Information(string message, params object[] args)
         {
-            return AddEntry(LogLevel.Warning, message);
+            return AddEntry(LogLevel.Information, message, null, args);
         }
 
-        public CommonLoggerProperties Fatal(string message)
+        public void LogInformation(string message, params object[] args)
         {
-            return AddEntry(LogLevel.Critical, message);
+            var buildMessage = AddEntry(LogLevel.Information, message, null, args);
+            buildMessage.Log();
         }
 
-        public CommonLoggerProperties Error(string message)
+        public CommonLoggerProperties Warning(string message, params object[] args)
         {
-            return AddEntry(LogLevel.Error, message);
+            return AddEntry(LogLevel.Warning, message, null, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            var buildMessage = AddEntry(LogLevel.Warning, message, null, args);
+            buildMessage.Log();
+        }
+
+        public CommonLoggerProperties Fatal(string message, params object[] args)
+        {
+            return AddEntry(LogLevel.Critical, message, null, args);
+        }
+
+        public void LogFatal(string message, params object[] args)
+        {
+            var buildMessage = AddEntry(LogLevel.Critical, message, null, args);
+            buildMessage.Log();
+        }
+
+        public CommonLoggerProperties Error(string message, params object[] args)
+        {
+            return AddEntry(LogLevel.Error, message, null, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            var buildMessage = AddEntry(LogLevel.Error, message, null, args);
+            buildMessage.Log();
         }
 
         public CommonLoggerProperties Exception(Exception exception)
@@ -52,9 +84,16 @@ namespace Arc4u.Diagnostics
             return property.AddStacktrace(exception.StackTrace ?? Environment.StackTrace);
         }
 
-        internal CommonLoggerProperties System(string message)
+        public void LogException(Exception exception)
         {
-            return AddEntry(LogLevel.Trace, message);
+            var property = AddEntry(LogLevel.Error, exception.Message, exception);
+            var buildMessage = property.AddStacktrace(exception.StackTrace ?? Environment.StackTrace);
+            buildMessage.Log();
+        }
+
+        internal CommonLoggerProperties System(string message, params object[] args)
+        {
+            return AddEntry(LogLevel.Trace, message, null, args);
         }
     }
 }
