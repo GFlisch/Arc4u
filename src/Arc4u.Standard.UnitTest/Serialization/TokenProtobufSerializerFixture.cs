31ff51bd1546019c41523ea50f47e946942595cb
Gilles Flisch <info@arc4u.net>
2022-05-29 12:55:56 +0200

    TokenInfo serialization Json and Protobuf

---
+++ src/Arc4u.Standard.UnitTest/Serialization/TokenProtobufSerializerFixture.cs
@@ -0,0 +1,22 @@
+﻿using Arc4u.Dependency;
+using Arc4u.OAuth2.Token;
+using Arc4u.Serializer;
+using Arc4u.Standard.UnitTest.Infrastructure;
+using Microsoft.Extensions.Configuration;
+
+namespace Arc4u.Standard.UnitTest.Serialization
+{
+    public class TokenProtobufSerializerFixture : ContainerFixture
+    {
+        public override string ConfigFile => @"Configs\Basic.json";
+
+        protected override void AddToContainer(IContainerRegistry container, IConfiguration configuration)
+        {
+            container.Register<IKeyValueSettings, MemorySettings>("Volatile");
+            container.Register<IObjectSerialization, ProtoBufSerialization>();
+            container.RegisterSingleton<ITokenCache, ApplicationCache>();
+            container.RegisterSingleton<CacheHelper, CacheHelper>();
+        }
+    }
+
+}
