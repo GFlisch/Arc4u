df8b43dbae0b556c7d5d35715f4b96cc817220c8
vvdb-architecture <66954369+vvdb-architecture@users.noreply.github.com>
2022-08-26 14:25:46 +0200

    Added OData support (#5)
    * Added OData support
    
    * Renamed types and use only .NET 5
    
    Co-authored-by: Vincent Van Den Berghe <VV0006@Belgrid.net>
    # Conflicts:
    #	src/Arc4u.sln

---
+++ src/Arc4u.Standard.OData/ODataExtensions.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Formatter;
+using Microsoft.AspNetCore.OData.Formatter.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Arc4u.Standard.OData
+{
+    /// <summary>
+    /// These extensions are required to make sure all URI's that our OData replies expose are relative to Yarp's address, not the address of our local service.
+    /// </summary>
+    public static class ODataExtensions
+    {
+        /// <summary>
+        /// Make sure that the service root URI used in OData serialization points to the endpoint used by external consumers (as understood by YARP).
+        /// OData can embed URI's to its own service in some scenarios, like pagination ("@odata.nextLink").
+        /// Call this method when setting up OData when configuring our service collection.
+        /// </summary>
+        /// <param name="configureServices">The service collection associated with the OData route components</param>
+        /// <param name="odataBaseAddress">
+        /// The base address of the service as seen by external consumers (i.e. from the point of view of YARP)
+        /// Can be null if the service is being tested standalone, not behind YARP.
+        /// </param>
+        /// <returns>The service collection passed as parameter</returns>
+        /// <example>
+        ///     services.AddControllers()
+        ///         ...
+        ///        .AddOData(options => options
+        ///            ...
+        ///            .AddRouteComponents("odata", ODataModel.GetEdmModel(), configureServices => configureServices.AddODataSerializerBaseAddress(odataBaseAddress))
+        ///            )
+        ///        ...
+        /// </example>
+        public static IServiceCollection AddODataSerializerBaseAddress(this IServiceCollection configureServices, Uri odataBaseAddress)
+        {
+            if (odataBaseAddress != null)
+                configureServices.AddSingleton((Func<IServiceProvider, IODataSerializerProvider>)(rootContainer => new ODataSerializerProvider(rootContainer, odataBaseAddress)));
+            return configureServices;
+        }
+
+
+        /// <summary>
+        /// Make sure OData's metadata URI's ("@odata.context") point to the endpoint used by external consumers (as understood by YARP).
+        /// </summary>
+        /// <param name="options">The MVC configuration options</param>
+        /// <param name="odataBaseAddress">
+        /// The base address of the service as seen by external consumers (i.e. from the point of view of YARP)
+        /// Can be null if the service is being tested standalone, not behind YARP.
+        /// </param>
+        /// <returns>The MVC configuration options passed as parameter</returns>
+        /// <example>
+        ///     services.AddControllers()
+        ///         ...
+        ///        .AddOData(...)
+        ///        ...
+        ///        .AddMvcOptions(options => options.SetODataFormattersBaseAddress(odataBaseAddress));
+        /// </example>
+        public static MvcOptions SetODataFormattersBaseAddress(this MvcOptions options, Uri odataBaseAddress)
+        {
+            // we need to provide an alternative base address for the OData metadata URI because we are behind a reverse proxy (YARP)
+            if (odataBaseAddress != null)
+            {
+                foreach (var outputFormatter in options.OutputFormatters.OfType<ODataOutputFormatter>())
+                    outputFormatter.BaseAddressFactory = request => odataBaseAddress;
+                foreach (var inputFormatter in options.InputFormatters.OfType<ODataInputFormatter>())
+                    inputFormatter.BaseAddressFactory = request => odataBaseAddress;
+            }
+            return options;
+        }
+    }
+}
---
+++ src/Arc4u.Standard.OData/ODataSerializerProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.OData.Extensions;
+using Microsoft.AspNetCore.OData.Formatter.Serialization;
+using System;
+
+namespace Arc4u.Standard.OData
+{
+    internal class ODataSerializerProvider: Microsoft.AspNetCore.OData.Formatter.Serialization.ODataSerializerProvider
+    {
+        private readonly Uri _odataBaseAddress;
+
+        public ODataSerializerProvider(IServiceProvider rootContainer, Uri odataBaseAddress)
+            : base(rootContainer)
+        {
+            _odataBaseAddress = odataBaseAddress;
+        }
+
+
+        public override IODataSerializer GetODataPayloadSerializer(Type type, HttpRequest request)
+        {
+            var odataFeature = request.ODataFeature();
+            var prefix = odataFeature.RoutePrefix + '/';
+            if (!_odataBaseAddress.LocalPath.EndsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                throw new ArgumentException($"The local path of the OData base address ({_odataBaseAddress.LocalPath}) does not end with the OData route prefix ({prefix})");
+            request.PathBase = _odataBaseAddress.LocalPath.Substring(0, _odataBaseAddress.LocalPath.Length - prefix.Length);
+            // this is the reason why any alternative base address can only change the scheme and the host, but never the whole path: otherwise, we would need to write our own payload serializer
+            // Not sure about the Path/PathBase would be a reliable option.
+            request.Scheme = _odataBaseAddress.Scheme;
+            request.Host = _odataBaseAddress.IsDefaultPort ? new HostString(_odataBaseAddress.Host) : new HostString(_odataBaseAddress.Host, _odataBaseAddress.Port);
+            return base.GetODataPayloadSerializer(type, request);
+        }
+    }
+}
