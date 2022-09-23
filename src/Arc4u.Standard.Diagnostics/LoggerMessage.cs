a7e5ac56d3e971e6c9068ede539855b9417cc930
Gilles Flisch <info@arc4u.net>
2022-09-23 23:15:33 +0200

    Refactor LoggerMessage to be compatible with version 5.0.13.

--- src/Arc4u.Standard.Diagnostics/LoggerMessage.cs
+++ src/Arc4u.Standard.Diagnostics/LoggerMessage.cs
@@ -18,10 +18,9 @@ namespace Arc4u.Diagnostics
             {
                 ProcessId = -1;
             }
-            
         }
 
-        private static int ProcessId { get; set; }
+        private static int ProcessId { get; }
         private readonly ILogger _logger;
 
         internal LoggerMessage(ILogger logger, MessageCategory category, string methodName, string typeClass)
@@ -38,29 +37,21 @@ namespace Arc4u.Diagnostics
         public string Text { get; set; }
         public string StackTrace { get; set; }
 
+
         public string MethodName { get; }
         public string TypeClass { get; }
         public object[] Args { get; set; }
 
+
         internal Dictionary<string, object> Properties { get; }
         internal Exception Exception { get; set; }
+
 
         internal void Log()
         {
             if (LogLevel < LoggerBase.FilterLevel) return;
 
             if (null == _logger) return;
-
-            // Used to extract the properties injected in the message and extracted from the internal struct Microsoft.Extensions.Logging.FormattedLogValues
-            var extractor = new StateExtractorLogger();
-            extractor.Log(LogLevel,
-                          0,
-                          Exception,
-                          Text,
-                          Args);
-
-            foreach(var property in extractor.Properties)
-                Properties.AddIfNotExist(property.Key, property.Value);
 
             if (null != LoggerContext.Current?.All())
             {
@@ -76,21 +67,25 @@ namespace Arc4u.Diagnostics
             Properties.AddIfNotExist(LoggingConstants.Category, (short)Category);
             Properties.AddIfNotExist(LoggingConstants.Stacktrace, StackTrace);
 
-
-            _logger.Log(LogLevel,
-                        0,
-                        Properties,
-                        Exception,
-                        (state, ex) => "");
+            // Add the internal Arc4u properties to whatever the TState provides already before logging
+            var stateLogger = new StateLogger(Properties, _logger);
+            stateLogger.Log(LogLevel, 0, Exception, Text, Args);
         }
     }
 
-    internal class StateExtractorLogger : ILogger
+
+
+    class StateLogger : ILogger
     {
-        public StateExtractorLogger()
+        private readonly IReadOnlyDictionary<string, object> _properties;
+        private readonly ILogger _logger;
+
+        public StateLogger(IReadOnlyDictionary<string, object> properties, ILogger logger)
         {
-            _properties = new();
+            _properties = properties;
+            _logger = logger;
         }
+
         public IDisposable BeginScope<TState>(TState state)
         {
             throw new NotImplementedException();
@@ -101,19 +96,26 @@ namespace Arc4u.Diagnostics
             throw new NotImplementedException();
         }
 
-        public IEnumerable<KeyValuePair<string, object>> Properties => _properties;
-
-        public string Message => _message;
-
-        private List<KeyValuePair<string, object>> _properties;
-        private string _message;
-
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
             if (state is IEnumerable<KeyValuePair<string, object>> pairs)
             {
-                _properties.AddRange(pairs);
+                Dictionary<string, object> mergedState = new();
+                foreach (KeyValuePair<string, object> pair in pairs)
+                    mergedState[pair.Key] = pair.Value;
+
+                foreach (var property in _properties)
+                    mergedState.AddIfNotExist(property.Key, property.Value);
+
+                _logger.Log(logLevel, eventId, mergedState, exception, LocalFormatter);
+
+                // we can do this since the template is not aware of the internal Arc4u properties.
+                string LocalFormatter(Dictionary<string, object> extendedState, Exception e) => formatter(state, exception);
             }
+            else
+                _logger.Log(logLevel, eventId, state, exception, formatter);
         }
     }
+
+
 }
