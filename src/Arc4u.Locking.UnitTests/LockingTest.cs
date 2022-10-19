cf81284872680ee63bcbcbd74fd95adddd03dd7d
Jan <jan@familie-schmidmaier.de>
2022-10-19 14:49:04 +0200

    further cleaning up

---
+++ src/Arc4u.Locking.UnitTests/LockingTest.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Threading.Tasks;
+using Arc4u.Locking.Abstraction;
+using Arc4u.Locking.Redis;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using StackExchange.Redis;
+using Xunit;
+
+namespace Arc4u.Locking.UnitTests;
+
+public class LockingTest
+{
+    private readonly Fixture _fixture = new();
+    [Fact]
+    public async Task Test1()
+    {
+
+        var lockingDl = new RedisLockingDataLayer(() => 
+            ConnectionMultiplexer.Connect(new ConfigurationOptions()
+            {
+                EndPoints = { "localhost: 6379" }
+            }), new NullLogger<RedisLockingDataLayer>());
+     
+        await NewMethod(lockingDl);
+        
+    }
+    
+    [Fact]
+    public async Task Test2()
+    {
+        var lockingDl = new MemoryLockingDataLayer();
+        await NewMethod(lockingDl);
+    }
+
+    private async Task NewMethod(ILockingDataLayer lockingDl)
+    {
+        var service = new LockingService(lockingDl, new LockingConfiguration(), new NullLogger<LockingService>());
+
+        var label = _fixture.Create<string>();
+
+        var firstTask = service.RunWithinLock(label, TimeSpan.FromMinutes(2),
+            async () => { await Task.Delay(TimeSpan.FromSeconds(3)); });
+
+        bool run = false;
+        var secondTask = service.RunWithinLock(label, TimeSpan.FromMinutes(2), async () =>
+        {
+            run = true;
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        });
+
+        await secondTask;
+
+        run.Should().BeFalse();
+        await firstTask;
+    }
+}
---
+++ src/Arc4u.Locking.UnitTests/MemoryLockingDataLayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Arc4u.Locking.Abstraction;
+
+namespace Arc4u.Locking.UnitTests;
+
+internal class MemoryLockingDataLayer : ILockingDataLayer
+{
+    private int _counter = 0;
+
+    private ReaderWriterLockSlim _lock = new();
+
+    public Task<Lock?> TryCreateLock(string label, TimeSpan maxAge)
+    {
+        _lock.EnterReadLock();
+        try
+        {
+            if (_counter > 0)
+                return Task.FromResult((Lock?) null);
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+
+        var timer = new Timer(state =>
+        {
+            _lock.EnterWriteLock();
+            _counter++;
+            _lock.ExitWriteLock();
+        }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+        var @lock = new Lock(() =>
+        {
+            _counter = 0;
+            return Task.CompletedTask;
+        }, () =>
+        {
+            timer.Dispose();
+            _lock.EnterWriteLock();
+            _counter = 0;
+            _lock.ExitWriteLock();
+        });
+        return Task.FromResult((Lock?) @lock);
+    }
+}
