f1f28df157fa34796655c505b79d092432320b07
Vincent Van Den Berghe <VV0006@Belgrid.net>
2022-10-19 18:26:52 +0200

    Refactored the various types of Json serializers with compression

---
+++ src/Arc4u.Standard.Serializer.JSon/JsonCompressedStreamSerializationBase.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.IO;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Arc4u.Serializer
+{
+    /// <summary>
+    /// Base class of Json serialization based on compressed streams.
+    /// </summary>
+    public abstract class JsonCompressedStreamSerializationBase
+    {
+        private readonly JsonSerializerOptions _options;
+        private readonly System.Text.Json.Serialization.JsonSerializerContext _context;
+
+        /// <summary>
+        /// For performance purposes, we use Microsoft's recyclable MemoryStrream pool
+        /// </summary>
+        private RecyclableMemoryStreamManager _recyclableMemoryStreamManager;
+
+        /// <summary>
+        /// Construct an instance with default options
+        /// </summary>
+        protected JsonCompressedStreamSerializationBase()
+        {
+        }
+
+        /// <summary>
+        /// Construct an instance, optionally specifying compression and other Json serializer options
+        /// </summary>
+        /// <param name="options">Json serializer options</param>
+        protected JsonCompressedStreamSerializationBase(JsonSerializerOptions options)
+        {
+            _options = options;
+        }
+
+
+        /// <summary>
+        /// Construct an instance, optionally specifying compression and a serialization context.
+        /// This is used for source generation, implemented in .NET 6 or later (https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation?pivots=dotnet-6-0)
+        /// </summary>
+        /// <param name="context">Json context for source generation</param>
+        protected JsonCompressedStreamSerializationBase(System.Text.Json.Serialization.JsonSerializerContext context)
+        {
+            _context = context;
+        }
+
+        protected abstract string SerializerType { get; }
+
+        protected virtual RecyclableMemoryStreamManager RecyclableMemoryStreamManager => _recyclableMemoryStreamManager ??= new RecyclableMemoryStreamManager();
+
+        protected void InternalSerialize<T>(Stream utf8json, T value)
+        {
+            if (_context != null)
+                JsonSerializer.Serialize(utf8json, value, typeof(T), _context);
+            else
+                JsonSerializer.Serialize(utf8json, value, _options);
+        }
+
+        protected T InternalDeserialize<T>(Stream utf8json)
+        {
+            if (_context != null)
+                return (T)JsonSerializer.Deserialize(utf8json, typeof(T), _context);
+            else
+                return JsonSerializer.Deserialize<T>(utf8json, _options);
+        }
+
+        protected object InternalDeserialize(Stream utf8json, Type returnType)
+        {
+            if (_context != null)
+                return JsonSerializer.Deserialize(utf8json, returnType, _context);
+            else
+                return JsonSerializer.Deserialize(utf8json, returnType, _options);
+        }
+    }
+}
--- src/Arc4u.Standard.Serializer.JSon/JsonSerialization.cs
+++ src/Arc4u.Standard.Serializer.JSon/JsonSerialization.cs
@@ -1,66 +1,74 @@
-﻿using Arc4u.Diagnostics;
-using System;
+﻿using System;
 using System.Diagnostics;
-using System.Text;
 using System.Text.Json;
 
 namespace Arc4u.Serializer
 {
+    /// <summary>
+    /// Implement object serialization with Json
+    /// </summary>
     public class JsonSerialization : IObjectSerialization
     {
+        private readonly JsonSerializerOptions _options;
+        private readonly System.Text.Json.Serialization.JsonSerializerContext _context;
+
+        /// <summary>
+        /// Construct an instance with default options
+        /// </summary>
+        public JsonSerialization()
+        {
+        }
+
+        /// <summary>
+        /// Construct an instance, with specific serializer options
+        /// </summary>
+        /// <param name="options">Json serializer options</param>
+        public JsonSerialization(JsonSerializerOptions options)
+        {
+            _options = options;
+        }
+
+
+        /// <summary>
+        /// Construct an instance with a serialization context.
+        /// This is used for source generation, implemented in .NET 6 or later (https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation?pivots=dotnet-6-0)
+        /// </summary>
+        /// <param name="context">Json context for source generation.</param>
+        public JsonSerialization(System.Text.Json.Serialization.JsonSerializerContext context)
+        {
+            _context = context;
+        }
+
+
         public byte[] Serialize<T>(T value)
         {
             Activity.Current?.SetTag("SerializerType", "Json");
 
-            string json = String.Empty;
-
-            switch (value)
-            {
-                case TimeSpan ts:
-                    json = JsonSerializer.Serialize(ts.Ticks);
-                    break;
-                default:
-                    json = JsonSerializer.Serialize(value);
-                    break;
-            }
-
-            return UTF8Encoding.UTF8.GetBytes(json);
-
+            if (_context != null)
+                return JsonSerializer.SerializeToUtf8Bytes(value, typeof(T), _context);
+            else
+                return JsonSerializer.SerializeToUtf8Bytes(value, _options);
         }
 
         public T Deserialize<T>(byte[] data)
         {
             Activity.Current?.SetTag("SerializerType", "Json");
 
-            var json = UTF8Encoding.UTF8.GetString(data);
-
-            var typo = typeof(T);
-            var type = IsNullable(typo) ? Nullable.GetUnderlyingType(typo).Name : typo.Name;
-            switch (type)
-            {
-                case "TimeSpan":
-                    long? ticks = JsonSerializer.Deserialize<long?>(json);
-                    return ticks.HasValue ? (T)(object)new TimeSpan(ticks.Value) : default(T);
-                default:
-                    return JsonSerializer.Deserialize<T>(json);
-            }
+            if (_context != null)
+                return (T)JsonSerializer.Deserialize(data, typeof(T), _context);
+            else
+                return JsonSerializer.Deserialize<T>(data, _options);
         }
 
-        private bool IsNullable(Type type) => Nullable.GetUnderlyingType(type) != null;
 
         public object Deserialize(byte[] data, Type objectType)
         {
-            var json = UTF8Encoding.UTF8.GetString(data);
+            Activity.Current?.SetTag("SerializerType", "Json");
 
-            var type = IsNullable(objectType) ? Nullable.GetUnderlyingType(objectType).Name : objectType.Name;
-            switch (type)
-            {
-                case "TimeSpan":
-                    long? ticks = JsonSerializer.Deserialize<long?>(json);
-                    return ticks.HasValue ? (object)new TimeSpan(ticks.Value) : null;
-                default:
-                    return JsonSerializer.Deserialize(json, objectType);
-            }
+            if (_context != null)
+                return JsonSerializer.Deserialize(data, objectType, _context);
+            else
+                return JsonSerializer.Deserialize(data, objectType, _options);
         }
     }
 }
