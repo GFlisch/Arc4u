0cdc219758de49065ab54ca0d3cc6215efff5366
Jan <jan@familie-schmidmaier.de>
2022-10-24 13:13:21 +0200

    reorganized tests

--- src/Arc4u.Locking.UnitTests/LockingTest.cs
+++ src/Arc4u.Locking.UnitTests/LockingTest.cs
@@ -2,72 +2,43 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Arc4u.Locking.Abstraction;
-using Arc4u.Locking.Redis;
 using AutoFixture;
 using FluentAssertions;
 using Microsoft.Extensions.Logging.Abstractions;
-using StackExchange.Redis;
 using Xunit;
 
 namespace Arc4u.Locking.UnitTests;
 
-public class LockingTest
+public abstract class LockingTest
 {
     private readonly Fixture _fixture = new();
-
+    
     [Fact]
-    [Trait("Dependency", "Redis")]
+    public async Task CreateLock_UsingRedis_SecondCallIsBlocked()
+    {
+        var lockingDl = BuildDataLayer();
+        await CreateLock_SecondCallIsBlocked(lockingDl);
+    }
+    
+    [Fact]
+    public async Task TryCreateLock_UsingRedis_SecondCallIsBlocked()
+    {
+        var lockingDl = BuildDataLayer();
+        await TryCreateLock_SecondCallIsBlocked(lockingDl);
+    }
+    [Fact]
+
     public async Task RunWithinLock_UsingRedis_SecondCallIsBlocked()
     {
-        var lockingDl = new RedisLockingDataLayer(() =>
-            ConnectionMultiplexer.Connect(new ConfigurationOptions()
-            {
-                EndPoints = {"localhost: 6379"}
-            }), new NullLogger<RedisLockingDataLayer>());
-
+        var lockingDl = BuildDataLayer();
+    
         await RunWithinLock_SecondCallIsBlocked(lockingDl);
     }
-
-    [Fact]
-    public async Task RunWithinLock_UsingInMemory_SecondCallIsBlocked()
-    {
-        var lockingDl = new MemoryLockingDataLayer();
-        await RunWithinLock_SecondCallIsBlocked(lockingDl);
-    }
-
-    private async Task RunWithinLock_SecondCallIsBlocked(ILockingDataLayer lockingDl)
-    {
-        var service = new LockingService(lockingDl, new LockingConfiguration(), new NullLogger<LockingService>());
-
-        var label = _fixture.Create<string>();
-
-        var firstTask = service.RunWithinLockAsync(label, TimeSpan.FromMinutes(2),
-            async () => { await Task.Delay(TimeSpan.FromSeconds(3)); }, CancellationToken.None);
-
-        bool run = false;
-        var secondTask = service.RunWithinLockAsync(label, TimeSpan.FromMinutes(2), async () =>
-            {
-                run = true;
-                await Task.Delay(TimeSpan.FromSeconds(1));
-            },
-            CancellationToken.None);
-
-        await secondTask;
-
-        run.Should().BeFalse();
-        await firstTask;
-    }
-
-
+        
     [Fact]
     public async Task RunWithinLock_UsingRedis_CancelFirstLockRunsSecond()
     {
-        var lockingDl = new RedisLockingDataLayer(() =>
-            ConnectionMultiplexer.Connect(new ConfigurationOptions()
-            {
-                EndPoints = {"localhost: 6379"}
-            }), new NullLogger<RedisLockingDataLayer>());
-
+        var lockingDl = BuildDataLayer();
         var service = new LockingService(lockingDl, new LockingConfiguration(), new NullLogger<LockingService>());
 
         var label = _fixture.Create<string>();
@@ -99,22 +70,29 @@ public class LockingTest
         await firstTask;
     }
 
-    [Fact]
-    public async Task CreateLock_UsingRedis_SecondCallIsBlocked()
+    private async Task RunWithinLock_SecondCallIsBlocked(ILockingDataLayer lockingDl)
     {
-        var lockingDl = new RedisLockingDataLayer(() =>
-            ConnectionMultiplexer.Connect(new ConfigurationOptions()
+        var service = new LockingService(lockingDl, new LockingConfiguration(), new NullLogger<LockingService>());
+
+        var label = _fixture.Create<string>();
+
+        var firstTask = service.RunWithinLockAsync(label, TimeSpan.FromMinutes(2),
+            async () => { await Task.Delay(TimeSpan.FromSeconds(3)); }, CancellationToken.None);
+
+        bool run = false;
+        var secondTask = service.RunWithinLockAsync(label, TimeSpan.FromMinutes(2), async () =>
             {
-                EndPoints = {"localhost: 6379"}
-            }), new NullLogger<RedisLockingDataLayer>());
-        await CreateLock_SecondCallIsBlocked(lockingDl);
+                run = true;
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            },
+            CancellationToken.None);
+
+        await secondTask;
+
+        run.Should().BeFalse();
+        await firstTask;
     }
 
-    [Fact]
-    public async Task CreateLock_UsingInMemory_SecondCallIsBlocked()
-    {
-        await CreateLock_SecondCallIsBlocked(new MemoryLockingDataLayer());
-    }
 
     private async Task CreateLock_SecondCallIsBlocked(ILockingDataLayer lockingDl)
     {
@@ -139,27 +117,6 @@ public class LockingTest
         await firstTask;
     }
 
-    [Fact]
-    public async Task TryCreateLock_UsingRedis_SecondCallIsBlocked()
-    {
-        var lockingDl = new RedisLockingDataLayer(() =>
-            ConnectionMultiplexer.Connect(new ConfigurationOptions()
-            {
-                EndPoints = {"localhost: 6379"}
-            }), new NullLogger<RedisLockingDataLayer>());
-
-        await TryCreateLock_SecondCallIsBlocked(lockingDl);
-    }
-    
-    [Fact]
-    public async Task TryCreateLock_UsingInMemory_SecondCallIsBlocked()
-    {
-        var lockingDl = new MemoryLockingDataLayer();
-
-        await TryCreateLock_SecondCallIsBlocked(lockingDl);
-    }
-    
-    
     private async Task TryCreateLock_SecondCallIsBlocked(ILockingDataLayer lockingDl)
     {
         var service = new LockingService(lockingDl, new LockingConfiguration(), new NullLogger<LockingService>());
@@ -180,4 +137,6 @@ public class LockingTest
         secondLock.Should().BeNull();
         await firstTask;
     }
+
+    protected abstract ILockingDataLayer BuildDataLayer();
 }
---
+++ src/Arc4u.Locking.UnitTests/RedisLockingTest.cs
@@ -0,0 +1,21 @@
+﻿using Arc4u.Locking.Abstraction;
+using Arc4u.Locking.Redis;
+using Microsoft.Extensions.Logging.Abstractions;
+using StackExchange.Redis;
+using Xunit;
+
+namespace Arc4u.Locking.UnitTests;
+[Trait("Dependency", "Redis")]
+public class RedisLockingTest : LockingTest
+{
+    protected override ILockingDataLayer BuildDataLayer()
+     {
+         var lockingDl = new RedisLockingDataLayer(() =>
+             ConnectionMultiplexer.Connect(new ConfigurationOptions()
+             {
+                 EndPoints = {"localhost: 6379"}
+             }), new NullLogger<RedisLockingDataLayer>());
+         return lockingDl;
+     }
+ 
+}
