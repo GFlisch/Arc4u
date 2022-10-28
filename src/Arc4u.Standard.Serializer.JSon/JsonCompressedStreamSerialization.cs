517336b6004d081daac1edb1c560d7b6cbed7322
vvdb-architecture <66954369+vvdb-architecture@users.noreply.github.com>
2022-10-28 11:23:19 +0200

    https://github.com/GFlisch/Arc4u.Guidance.Doc/issues/51 (#10)
    * https://github.com/GFlisch/Arc4u.Guidance.Doc/issues/51
    
    More performant Json (compressed) serialization.
    
    * Refactored the various types of Json serializers with compression
    
    * Corrected syntax error in comment
    
    * Update CompressionAndSpeedTests.cs
    
    Issue with Zip and Protobuf when using a bearer token generated by code.
    
    * Corrected bug while deserializing from a Zip archive, where the stored bytes were not completely read.
    
    Co-authored-by: Vincent Van Den Berghe <VV0006@Belgrid.net>
    Co-authored-by: GFlisch <github@flisch.be>

---
+++ src/Arc4u.Standard.Serializer.JSon/JsonCompressedStreamSerialization.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.IO;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+
+namespace Arc4u.Serializer
+{
+    /// <summary>
+    /// Base class of Json serialization based on compressed streams.
+    /// </summary>
+    public abstract class JsonCompressedStreamSerialization : JsonCompressedStreamSerializationBase, IObjectSerialization
+    {
+        /// <summary>
+        /// Construct with default options
+        /// </summary>
+        protected JsonCompressedStreamSerialization()
+        {
+        }
+
+        /// <summary>
+        /// Construct an instance, optionally specifying compression and other Json serializer options
+        /// </summary>
+        /// <param name="options">Json serializer options</param>
+        protected JsonCompressedStreamSerialization(JsonSerializerOptions options)
+            :base(options)
+        {
+        }
+
+
+        /// <summary>
+        /// Construct an instance, optionally specifying compression and a serialization context.
+        /// This is used for source generation, implemented in .NET 6 or later (https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation?pivots=dotnet-6-0)
+        /// </summary>
+        /// <param name="context">Json context for source generation</param>
+        protected JsonCompressedStreamSerialization(System.Text.Json.Serialization.JsonSerializerContext context)
+            :base(context)
+        {
+        }
+
+        protected abstract Stream CreateForCompression(Stream stream);
+
+        protected abstract Stream CreateForDecompression(Stream stream);
+
+        public byte[] Serialize<T>(T value)
+        {
+            Activity.Current?.SetTag("SerializerType", SerializerType);
+
+            using (var output = RecyclableMemoryStreamManager.GetStream())
+            {
+                using (var compressed = CreateForCompression(output))
+                    InternalSerialize(compressed, value);
+                return output.ToArray();
+            }
+        }
+
+        public T Deserialize<T>(byte[] data)
+        {
+            Activity.Current?.SetTag("SerializerType", SerializerType);
+
+            using (var compressed = RecyclableMemoryStreamManager.GetStream(data))
+            using (var uncompressed = CreateForDecompression(compressed))
+                return InternalDeserialize<T>(uncompressed);
+        }
+
+
+        public object Deserialize(byte[] data, Type objectType)
+        {
+            Activity.Current?.SetTag("SerializerType", SerializerType);
+
+            using (var compressed = RecyclableMemoryStreamManager.GetStream(data))
+            using (var uncompressed = CreateForDecompression(compressed))
+                return InternalDeserialize(uncompressed, objectType);
+        }
+    }
+}
---
+++ src/Arc4u.Standard.Serializer.JSon/JsonDeflateSerialization.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text.Json;
+
+namespace Arc4u.Serializer
+{
+    /// <summary>
+    /// Implement object serialization with Json and Deflate compression (https://en.wikipedia.org/wiki/Deflate).
+    /// </summary>
+    public class JsonDeflateSerialization : JsonCompressedStreamSerialization
+    {
+        /// <summary>
+        /// Construct an instance with default options
+        /// </summary>
+        public JsonDeflateSerialization()
+        {
+        }
+
+        /// <summary>
+        /// Construct an instance, with specific serializer options
+        /// </summary>
+        /// <param name="options">Json serializer options.</param>
+        public JsonDeflateSerialization(JsonSerializerOptions options)
+            : base(options)
+        {
+        }
+
+
+        /// <summary>
+        /// Construct an instance with a serialization context.
+        /// This is used for source generation, implemented in .NET 6 or later (https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation?pivots=dotnet-6-0)
+        /// </summary>
+        /// <param name="context">Json context for source generation.</param>
+        public JsonDeflateSerialization(System.Text.Json.Serialization.JsonSerializerContext context)
+            : base(context)
+        {
+        }
+
+        protected override string SerializerType => "Json+DeflateCompression";
+
+        protected override Stream CreateForCompression(Stream stream)
+        {
+            return new DeflateStream(stream, CompressionLevel.Fastest, leaveOpen: true);
+        }
+
+        protected override Stream CreateForDecompression(Stream stream)
+        {
+            return new DeflateStream(stream, CompressionMode.Decompress);
+        }
+    }
+}
