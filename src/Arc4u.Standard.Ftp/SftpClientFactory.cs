4610fe3f4a85c1659f669716033b9d2f2f5f1e20
Jan <jan@familie-schmidmaier.de>
2022-11-23 11:36:41 +0100

    added more documentation

--- src/Arc4u.Standard.Ftp/SftpClientFactory.cs
+++ src/Arc4u.Standard.Ftp/SftpClientFactory.cs
@@ -1,22 +1,36 @@
-﻿using System;
-using Arc4u.Diagnostics;
+﻿using Arc4u.Diagnostics;
 using Arc4u.Network.Pooling;
 using Microsoft.Extensions.Logging;
 using Renci.SshNet;
 
 namespace Arc4u.Standard.Ftp
 {
+    /// <summary>
+    /// Factory to create <see cref="PoolableItem"/> in form of an SftpCLient
+    /// </summary>
     public class SftpClientFactory : IClientFactory<SftpClientFacade>
     {
         private readonly ILogger<SftpClientFactory> _logger;
         private readonly IFtpConfiguration _ftpConfig;
-
+        
+        /// <summary>
+        /// Instantiates a new factory 
+        /// </summary>
+        /// <param name="ftpConfig">configuration to be used when creating new poolable sftp clients</param>
+        /// <param name="logger">Logger for this factory</param>
         public SftpClientFactory (IFtpConfiguration ftpConfig, ILogger<SftpClientFactory> logger)
         {
             _ftpConfig = ftpConfig;
             _logger = logger;
         }
 
+        /// <summary>
+        /// Builds a new poolable sftp client, by using the provided configuration of the ctor
+        /// </summary>
+        /// <returns>poolable sftp client</returns>
+        /// <remarks>
+        /// The created internally created client is using a BasicAuth (username & password) and has a keep alive interval activated. The provided client is already connected to the sftp server
+        /// </remarks>
         public SftpClientFacade CreateClient()
         {
             var c = new SftpClient(new ConnectionInfo(_ftpConfig.Host, _ftpConfig.Username, new PasswordAuthenticationMethod(_ftpConfig.Username, _ftpConfig.Password)));
@@ -26,7 +40,6 @@ namespace Arc4u.Standard.Ftp
             _logger.Technical().Debug("Connected.").Log();
             return new SftpClientFacade(c);
         }
-
     }
 }
 
--- src/Arc4u.Standard.Ftp/StartupExtension.cs
+++ src/Arc4u.Standard.Ftp/StartupExtension.cs
@@ -2,30 +2,73 @@
 using Arc4u.Extensions;
 using Arc4u.Network.Pooling;
 using Arc4u.Security.Cryptography;
+using JetBrains.Annotations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Arc4u.Standard.Ftp;
 
+[PublicAPI]
 public static class StartupExtension
 {
+    /// <summary>
+    ///     Registering everything needed to use an sFTP pooling
+    /// </summary>
+    /// <param name="serviceCollection">serviceCollection to be used for registration</param>
+    /// <returns>provided <see cref="ServiceCollection" /> in parameter <paramref name="serviceCollection" /></returns>
+    /// <remarks>
+    ///     The configuration being used internally is the <see cref="FtpConfiguration" />. This will be registered within
+    ///     the <paramref name="serviceCollection" /> as <see cref="IFtpConfiguration" />
+    /// </remarks>
     public static IServiceCollection UseFtpPooling(this IServiceCollection serviceCollection)
     {
         return UseFtpPooling<FtpConfiguration>(serviceCollection);
     }
 
-
+    /// <summary>
+    ///     Registering everything needed to use an sFTP pooling
+    /// </summary>
+    /// <typeparam name="TConfiguration">
+    ///     Type of ftp configuration to be used and registered within the
+    ///     <paramref name="serviceCollection" /> as <see cref="IFtpConfiguration" />
+    /// </typeparam>
+    /// <param name="serviceCollection">serviceCollection to be used for registration</param>
+    /// <returns>provided <see cref="ServiceCollection" /> in parameter <paramref name="serviceCollection" /></returns>
+    /// <remarks>
+    ///     The configuration will be read and registered  within the <paramref name="serviceCollection" /> as
+    ///     <see cref="IFtpConfiguration" />
+    /// </remarks>
     public static IServiceCollection UseFtpPooling<TConfiguration>(this IServiceCollection serviceCollection)
         where TConfiguration : IFtpConfiguration, new()
     {
         serviceCollection.AddSingleton<IFtpConfiguration>(provider =>
         {
             var decryptionProvider = provider.GetService<IDecryptionProvider>();
-            return (provider.GetService<IConfiguration>() ?? throw new InvalidOperationException($"{typeof(IConfiguration)} not found within the DI container, although it was ")).GetAndDecrypt<TConfiguration>(decryptionProvider);
+            return (provider.GetService<IConfiguration>() ??
+                    throw new InvalidOperationException(
+                        $"{typeof(IConfiguration)} not found within the DI container, although it was "))
+                .GetAndDecrypt<TConfiguration>(decryptionProvider);
         });
         return RegisterServices(serviceCollection);
     }
 
+    /// <summary>
+    ///     Registering everything needed to use an sFTP pooling
+    /// </summary>
+    /// <param name="serviceCollection">serviceCollection to be used for registration</param>
+    /// <param name="configuration">ftp configuration to be used and registered for the ftp pooling</param>
+    /// <returns>provided <see cref="ServiceCollection" /> in parameter <paramref name="serviceCollection" /></returns>
+    /// <remarks>
+    ///     The configuration will registered  within the <paramref name="serviceCollection" /> as
+    ///     <see cref="IFtpConfiguration" />
+    /// </remarks>
+    public static IServiceCollection UseFtpPooling(this IServiceCollection serviceCollection,
+        IFtpConfiguration configuration)
+    {
+        serviceCollection.AddSingleton(configuration);
+        return RegisterServices(serviceCollection);
+    }
+    
     private static IServiceCollection RegisterServices(IServiceCollection serviceCollection)
     {
         serviceCollection.AddSingleton<IClientFactory<SftpClientFacade>, SftpClientFactory>();
@@ -33,10 +76,4 @@ public static class StartupExtension
         return serviceCollection;
     }
 
-    public static IServiceCollection UseFtpPooling<TConfiguration>(this IServiceCollection serviceCollection,
-        IFtpConfiguration configuration)
-    {
-        serviceCollection.AddSingleton(configuration);
-        return RegisterServices(serviceCollection);
-    }
 }
