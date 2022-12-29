2aaf7a4747dfb5dc4fefc6369582de1c7b3cca52
Dehu Frédéric <Frederic.Dehu@externel.be>
2022-12-29 12:11:38 +0100

    Fix concurrency issue in AppPrincipalFactory Cleanup KeyGeneratorFromIdentity

--- src/Arc4u.Standard.OAuth2/Configuration/UserConfig.cs
+++ src/Arc4u.Standard.OAuth2/Configuration/UserConfig.cs
@@ -6,11 +6,6 @@ namespace Arc4u.OAuth2.Configuration
     public class UserConfig
     {
         /// <summary>
-        /// The name of the cache used to retrieve the instance of the cache => CacheContext.
-        /// </summary>
-        public String Identifier { get; set; } = "UniqueId";
-
-        /// <summary>
         /// The claim type used to retrieve the STS identifier for the user.
         /// </summary>
         public List<String> Claims { get; set; } = new List<string>();
