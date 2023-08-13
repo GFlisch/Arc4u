aaf7cfbd6d3aec720954d2d51f2c8a3949ca7009
GFlisch <info@arc4u.net>
2023-08-13 17:56:50 +0200

    Add FluentResult

---
+++ src/Arc4u.Standard.Core/ValueObject.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arc4u.Core;
+
+public abstract class ValueObject
+{
+    protected static bool EqualOperator(ValueObject? left, ValueObject? right)
+    {
+        if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+        {
+            return false;
+        }
+
+        return null == left ? ReferenceEquals(left, right) : ReferenceEquals(left, right) || left.Equals(right);
+    }
+
+    protected static bool NotEqualOperator(ValueObject? left, ValueObject? right)
+    {
+        return !(EqualOperator(left, right));
+    }
+
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+
+        return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+                .Select(x => x != null ? x.GetHashCode() : 0)
+                .Aggregate((x, y) => x ^ y);
+    }
+    public static bool operator ==(ValueObject? one, ValueObject? two)
+    {
+        return EqualOperator(one, two);
+    }
+
+    public static bool operator !=(ValueObject? one, ValueObject? two)
+    {
+        return NotEqualOperator(one, two);
+    }
+}
+
