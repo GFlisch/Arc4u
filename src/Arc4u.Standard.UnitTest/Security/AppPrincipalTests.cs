a1200a644627f3c8c51aff1e400dd21a2141a270
GFlisch <info@arc4u.net>
2023-11-15 17:36:48 +0100

    Add IsAuthorized when access is defined as an Enum!

--- src/Arc4u.Standard.UnitTest/Security/AppPrincipalTests.cs
+++ src/Arc4u.Standard.UnitTest/Security/AppPrincipalTests.cs
@@ -1,6 +1,12 @@
 using AutoFixture.AutoMoq;
 using AutoFixture;
 using Xunit;
+using Arc4u.Security.Principal;
+using System.Collections.Generic;
+using System.Linq;
+using Arc4u.Extensions;
+using Arc4u.OAuth2.AspNetCore.Options;
+using FluentAssertions;
 
 namespace Arc4u.UnitTest.Security;
 
@@ -15,9 +21,55 @@ public class AppPrincipalTests
 
     private readonly Fixture _fixture;
 
-    //[Fact]
-    //public Test_IsAuthorise_By_Int_Should()
-    //{
+    private enum Access : int
+    {
+        AccessApplication = 1,
+        CanSeeSwaggerFacadeApi = 2
+    }
 
-    //}
+    [Fact]
+    public void Test_IsAuthorise_By_Enum_Should()
+    {
+        var authorization = GetAuthorization();
+
+        var appAuthorization = new AppAuthorization(authorization);
+
+        appAuthorization.IsAuthorized(Access.AccessApplication).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test_IsAuthorise_By_Enum_Should_Not()
+    {
+        var authorization = GetAuthorization();
+
+        var appAuthorization = new AppAuthorization(authorization);
+
+        appAuthorization.IsAuthorized(Access.AccessApplication, Access.CanSeeSwaggerFacadeApi).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test_IsAuthorise_By_Enum_Should_For_Specific()
+    {
+        var authorization = GetAuthorization();
+
+        var appAuthorization = new AppAuthorization(authorization);
+
+        appAuthorization.IsAuthorized("Specific", Access.AccessApplication, Access.CanSeeSwaggerFacadeApi).Should().BeTrue();
+    }
+
+    private static Authorization GetAuthorization()
+    {
+        var defaultScopedOperations = new ScopedOperations { Operations = new List<int> { (int)Access.AccessApplication }, Scope = "" };
+        var specificScope = new ScopedOperations { Operations = new List<int> { (int)Access.AccessApplication, (int)Access.CanSeeSwaggerFacadeApi }, Scope = "Specific" };
+        var authorization = new Authorization
+        {
+            Operations = new List<ScopedOperations> { defaultScopedOperations, specificScope },
+            AllOperations = AllOperations,
+            Roles = new List<ScopedRoles> { new ScopedRoles { Roles = new List<string> { "User" }, Scope = "" } },
+            Scopes = new List<string> { "", "Specific" }
+        };
+
+        return authorization;
+    }
+    private static List<Operation> AllOperations => System.Enum.GetValues<Access>().Select(o => new Operation { Name = o.GetValue(), ID = (int)o }).ToList();
 }
--- src/Arc4u.Standard/Security/Principal/AppAuthorization.cs
+++ src/Arc4u.Standard/Security/Principal/AppAuthorization.cs
@@ -1,164 +1,188 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Arc4u.Security.Principal
+namespace Arc4u.Security.Principal;
+
+public class AppAuthorization : IAuthorization
 {
-    public class AppAuthorization : IAuthorization
+    private readonly Dictionary<string, Dictionary<int, string>> _operations;
+    private readonly Dictionary<string, Dictionary<string, int>> _operationsName;
+    private readonly Dictionary<string, Dictionary<string, short>> _roles;
+    private readonly List<String> _scopes;
+
+    public AppAuthorization(Authorization authorizationData)
     {
-        public const string Key = "{3498805A-7C29-4f4c-A463-F2083AF48032}";
-
-        private readonly Dictionary<string, Dictionary<int, string>> _operations;
-        private readonly Dictionary<string, Dictionary<string, int>> _operationsName;
-        private readonly Dictionary<string, Dictionary<string, short>> _roles;
-        private readonly List<String> _scopes;
-
-        public AppAuthorization(Authorization authorizationData)
+        // Fill the Dictionnary structure for fast retrieving the information.
+        // Add Operations
+        _operations = new Dictionary<string, Dictionary<int, string>>();
+        _operationsName = new Dictionary<string, Dictionary<string, int>>();
+        foreach (var scopedOperations in authorizationData.Operations)
         {
-            // Fill the Dictionnary structure for fast retrieving the information.
-            // Add Operations
-            _operations = new Dictionary<string, Dictionary<int, string>>();
-            _operationsName = new Dictionary<string, Dictionary<string, int>>();
-            foreach (var scopedOperations in authorizationData.Operations)
-            {
-                var operations = new Dictionary<int, string>();
-                var operationsName = new Dictionary<string, int>();
+            var operations = new Dictionary<int, string>();
+            var operationsName = new Dictionary<string, int>();
 
-                foreach (Int32 operationId in scopedOperations.Operations)
+            foreach (Int32 operationId in scopedOperations.Operations)
+            {
+                var operation = authorizationData.AllOperations.SingleOrDefault(o => o.ID == operationId);
+                if (default(Operation) != operation)
                 {
-                    var operation = authorizationData.AllOperations.SingleOrDefault(o => o.ID == operationId);
-                    if (default(Operation) != operation)
-                    {
-                        operations.Add(operation.ID, operation.Name);
-                        operationsName.Add(operation.Name, operation.ID);
-                    }
-                };
-                _operations.Add(scopedOperations.Scope, operations);
-                _operationsName.Add(scopedOperations.Scope, operationsName);
-            }
-
-            // Add Roles.
-            _roles = new Dictionary<string, Dictionary<string, short>>();
-            foreach (var scopedRoles in authorizationData.Roles)
-            {
-                var roles = new Dictionary<string, short>();
-                foreach (var role in scopedRoles.Roles) roles.Add(role, 0);
-
-                _roles.Add(scopedRoles.Scope, roles);
-            }
-
-            _scopes = authorizationData.Scopes;
-        }
-
-        #region IAuthorization Members
-
-        public string AuthorizationType
-        {
-            get { return "Arc4uAuthorization"; }
-        }
-
-        public string[] Scopes()
-        {
-            return _scopes.ToArray();
-        }
-
-        public string[] Roles()
-        {
-            return Roles(string.Empty);
-        }
-
-        public string[] Roles(string scope)
-        {
-            try
-            {
-                var roles = new string[_roles[scope].Count];
-                _roles[scope].Keys.CopyTo(roles, 0);
-
-                return roles;
-            }
-            catch
-            {
-                return new string[0];
-            }
-        }
-
-        public bool IsAuthorized(params int[] operations)
-        {
-            return IsAuthorized(string.Empty, operations);
-        }
-
-        public bool IsAuthorized(params String[] operations)
-        {
-            return IsAuthorized(string.Empty, operations);
-        }
-
-        public bool IsAuthorized(string scope, params int[] operations)
-        {
-            if (_operations.ContainsKey(scope))
-            {
-                foreach (int i in operations)
-                {
-                    if (!_operations[scope].ContainsKey(i))
-                        return false;
+                    operations.Add(operation.ID, operation.Name);
+                    operationsName.Add(operation.Name, operation.ID);
                 }
-            }
-            else // No Scope no Operations.
-                return false;
-
-            return true;
+            };
+            _operations.Add(scopedOperations.Scope, operations);
+            _operationsName.Add(scopedOperations.Scope, operationsName);
         }
 
-        public bool IsAuthorized(string scope, params string[] operations)
+        // Add Roles.
+        _roles = new Dictionary<string, Dictionary<string, short>>();
+        foreach (var scopedRoles in authorizationData.Roles)
         {
-            if (_operationsName.ContainsKey(scope))
+            var roles = new Dictionary<string, short>();
+            foreach (var role in scopedRoles.Roles)
             {
-                foreach (string o in operations)
-                {
-                    if (!_operationsName[scope].ContainsKey(o))
-                        return false;
-                }
-
+                roles.Add(role, 0);
             }
-            else // No Scope no Operations.
-                return false;
 
-            return true;
+            _roles.Add(scopedRoles.Scope, roles);
         }
 
-
-        public bool IsInRole(string role)
-        {
-            return IsInRole(string.Empty, role);
-        }
-
-        public bool IsInRole(string scope, string role)
-        {
-            if (!_roles.ContainsKey(scope))
-                return false;
-
-            return _roles[scope].ContainsKey(role);
-        }
-
-        public string[] Operations()
-        {
-            return Operations(string.Empty);
-        }
-
-        public string[] Operations(string scope)
-        {
-            try
-            {
-                var operations = new string[_operations[scope].Count];
-                _operations[scope].Values.CopyTo(operations, 0);
-
-                return operations;
-            }
-            catch
-            {
-                return new string[0];
-            }
-        }
-
-        #endregion
+        _scopes = authorizationData.Scopes;
     }
+
+    #region IAuthorization Members
+
+    public string AuthorizationType
+    {
+        get { return "Arc4uAuthorization"; }
+    }
+
+    public string[] Scopes()
+    {
+        return _scopes.ToArray();
+    }
+
+    public string[] Roles()
+    {
+        return Roles(string.Empty);
+    }
+
+    public string[] Roles(string scope)
+    {
+        try
+        {
+            var roles = new string[_roles[scope].Count];
+            _roles[scope].Keys.CopyTo(roles, 0);
+
+            return roles;
+        }
+        catch
+        {
+            return new string[0];
+        }
+    }
+
+    public bool IsAuthorized(params int[] operations)
+    {
+        return IsAuthorized(string.Empty, operations);
+    }
+
+    public bool IsAuthorized(params String[] operations)
+    {
+        return IsAuthorized(string.Empty, operations);
+    }
+
+    public bool IsAuthorized(string scope, params int[] operations)
+    {
+        if (_operations.ContainsKey(scope))
+        {
+            foreach (int i in operations)
+            {
+                if (!_operations[scope].ContainsKey(i))
+                {
+                    return false;
+                }
+            }
+        }
+        else // No Scope no Operations.
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool IsAuthorized(string scope, params string[] operations)
+    {
+        if (_operationsName.ContainsKey(scope))
+        {
+            foreach (string o in operations)
+            {
+                if (!_operationsName[scope].ContainsKey(o))
+                {
+                    return false;
+                }
+            }
+
+        }
+        else // No Scope no Operations.
+        {
+            return false;
+        }
+
+        return true;
+    }
+    public bool IsAuthorized<TAccess>(params TAccess[] operations) where TAccess : struct, Enum
+    {
+        return IsAuthorized(string.Empty, operations);
+    }
+
+    public bool IsAuthorized<TAccess>(string scope, params TAccess[] operations)
+           where TAccess : struct, Enum
+    {
+        var ids = new int[operations.Length];
+
+        for (int index = 0; index < operations.Length; ++index)
+        {
+            ids[index] = Convert.ToInt32(operations[index]);
+        }
+
+        return IsAuthorized(scope, ids);
+    }
+
+    public bool IsInRole(string role)
+    {
+        return IsInRole(string.Empty, role);
+    }
+
+    public bool IsInRole(string scope, string role)
+    {
+        if (!_roles.ContainsKey(scope))
+            return false;
+
+        return _roles[scope].ContainsKey(role);
+    }
+
+    public string[] Operations()
+    {
+        return Operations(string.Empty);
+    }
+
+    public string[] Operations(string scope)
+    {
+        try
+        {
+            var operations = new string[_operations[scope].Count];
+            _operations[scope].Values.CopyTo(operations, 0);
+
+            return operations;
+        }
+        catch
+        {
+            return new string[0];
+        }
+    }
+
+    #endregion
 }
