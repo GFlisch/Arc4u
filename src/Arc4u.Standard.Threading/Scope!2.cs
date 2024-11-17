48efa8be6f513d76a718e16e92c24a4604211915
Gilles Flisch <info@arc4u.net>
2024-11-17 23:58:12 +0100

    Scope2

--- src/Arc4u.Standard.Threading/Scope!2.cs
+++ src/Arc4u.Standard.Threading/Scope!2.cs
@@ -1,111 +1,110 @@
-﻿using System;
-using System.Threading;
+using System.Globalization;
 
-namespace Arc4u.Threading
+namespace Arc4u.Threading;
+
+/// <summary>
+/// Scopes a type instance.
+/// </summary>
+/// <typeparam name="TScope">The type of the scope.</typeparam>
+/// <typeparam name="TInstance">The type of the instance.</typeparam>
+public class Scope<TScope, TInstance> : IDisposable
 {
+    private static readonly AsyncLocal<Scope<TScope, TInstance>?> _instance = new AsyncLocal<Scope<TScope, TInstance>?>();
+
+    private bool Disposed { get; set; }
+    private bool ToDispose { get; set; }
+    private Scope<TScope, TInstance>? Parent { get; set; }
+
+    private TInstance Value;
+
     /// <summary>
-    /// Scopes a type instance.
+    /// Prevents a default instance of the <see cref="Scope&lt;T&gt;"/> class from being created.
     /// </summary>
-    /// <typeparam name="TScope">The type of the scope.</typeparam>
-    /// <typeparam name="TInstance">The type of the instance.</typeparam>
-    public class Scope<TScope, TInstance> : IDisposable
+    private Scope()
     {
-        //private static AsyncLocal<T> _instance = new AsyncLocal<T>();
-        private static AsyncLocal<Scope<TScope, TInstance>> _instance = new AsyncLocal<Scope<TScope, TInstance>>();
+        Value = default!;
+    }
 
-        private bool Disposed { get; set; }
-        private bool ToDispose { get; set; }
-        private Scope<TScope, TInstance> Parent { get; set; }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Scope&lt;T&gt;"/> class.
+    /// </summary>
+    /// <param name="instance">The instance.</param>
+    public Scope(TInstance instance)
+        : this(instance, true)
+    {
+    }
 
-        private TInstance Value = default(TInstance);
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Scope&lt;T&gt;"/> class.
+    /// </summary>
+    /// <param name="instance">The instance.</param>
+    /// <param name="dispose">Indicates if the scoped <paramref name="instance"/> is disposed when its scope is disposed.</param>
+    public Scope(TInstance instance, bool toDispose)
+    {
+        Value = instance;
+        Parent = _instance.Value;
+        _instance.Value = this;
+        ToDispose = toDispose;
+    }
 
-        /// <summary>
-        /// Prevents a default instance of the <see cref="Scope&lt;T&gt;"/> class from being created.
-        /// </summary>
-        protected Scope()
+    /// <summary>
+    /// Gets the current instance of T in the ambient scope.
+    /// </summary>
+    /// <value>The current instance of T.</value>
+    public static TInstance? Current
+    {
+        get
         {
+            return null == _instance.Value ? default(TInstance) : _instance.Value.Value;
         }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Scope&lt;T&gt;"/> class.
-        /// </summary>
-        /// <param name="instance">The instance.</param>
-        public Scope(TInstance instance)
-            : this(instance, true)
+        set
         {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Scope&lt;T&gt;"/> class.
-        /// </summary>
-        /// <param name="instance">The instance.</param>
-        /// <param name="dispose">Indicates if the scoped <paramref name="instance"/> is disposed when its scope is disposed.</param>
-        public Scope(TInstance instance, bool toDispose)
-        {
-            Value = instance;
-            Parent = _instance.Value;
-            _instance.Value = this;
-            ToDispose = toDispose;
-        }
-
-        /// <summary>
-        /// Gets the current instance of T in the ambient scope.
-        /// </summary>
-        /// <value>The current instance of T.</value>
-        public static TInstance Current
-        {
-            get
-            {
-                return null == _instance.Value ? default(TInstance) : _instance.Value.Value;
-            }
-            set
+            if (_instance.Value != null && null != value)
             {
                 _instance.Value.Value = value;
             }
         }
-
-        protected TInstance ParentValue
-        {
-            get { return null == Parent ? default(TInstance) : Parent.Value; }
-        }
-
-        protected static Scope<TScope, TInstance> Ambient
-        {
-            get { return _instance.Value; }
-        }
-
-        /// <summary>
-        /// Disposes the ambient scope and its current instance when applicable.
-        /// </summary>
-        public virtual void Dispose()
-        {
-            if (!Disposed)
-            {
-                Disposed = true;
-
-                if (ToDispose)
-                {
-                    var disposable = Current as IDisposable;
-                    if (disposable != null)
-                        disposable.Dispose();
-                }
-            }
-
-            _instance.Value = Parent;
-        }
-
-        /// <summary>
-        /// Returns a <see cref="System.String"/> that represents this instance.
-        /// </summary>
-        /// <returns>
-        /// A <see cref="System.String"/> that represents this instance.
-        /// </returns>
-        public override string ToString()
-        {
-            return Current != null
-                ? string.Format("Scoping: {0}", Current)
-                : base.ToString();
-        }
     }
 
+    protected TInstance? ParentValue
+    {
+        get { return null == Parent ? default(TInstance) : Parent.Value; }
+    }
+
+    protected static Scope<TScope, TInstance>? Ambient
+    {
+        get { return _instance.Value; }
+    }
+
+    /// <summary>
+    /// Disposes the ambient scope and its current instance when applicable.
+    /// </summary>
+    public void Dispose()
+    {
+        if (!Disposed)
+        {
+            Disposed = true;
+
+            if (ToDispose)
+            {
+                var disposable = Current as IDisposable;
+                disposable?.Dispose();
+            }
+        }
+
+        _instance.Value = Parent;
+    }
+
+    /// <summary>
+    /// Returns a <see cref="System.String"/> that represents this instance.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="System.String"/> that represents this instance.
+    /// </returns>
+    public override string ToString()
+    {
+        return Current != null
+            ? string.Format(CultureInfo.InvariantCulture, "Scoping: {0}", Current)
+            : base.ToString()!;
+    }
 }
---
+++ src/Arc4u.Standard.UnitTest/Threading/Scope2Tests.cs
@@ -0,0 +1,66 @@
+using Arc4u.Threading;
+using Xunit;
+
+namespace Arc4u.UnitTest.Threading;
+
+public class Scope2Tests
+{
+    [Fact]
+    public void TestScopeInitialization()
+    {
+        var instance = new TestInstance();
+        using (var scope = new Scope<string, TestInstance>(instance))
+        {
+            Assert.Equal(instance, Scope<string, TestInstance>.Current);
+        }
+    }
+
+    [Fact]
+    public void TestScopeDisposal()
+    {
+        var instance = new TestInstance();
+        using (var scope = new Scope<string, TestInstance>(instance))
+        {
+            Assert.Equal(instance, Scope<string, TestInstance>.Current);
+        }
+        Assert.Null(Scope<string, TestInstance>.Current);
+    }
+
+    [Fact]
+    public void TestNestedScopes()
+    {
+        var instance1 = new TestInstance();
+        var instance2 = new TestInstance();
+
+        using (var scope1 = new Scope<string, TestInstance>(instance1))
+        {
+            Assert.Equal(instance1, Scope<string, TestInstance>.Current);
+
+            using (var scope2 = new Scope<string, TestInstance>(instance2))
+            {
+                Assert.Equal(instance2, Scope<string, TestInstance>.Current);
+            }
+
+            Assert.Equal(instance1, Scope<string, TestInstance>.Current);
+        }
+        Assert.Null(Scope<string, TestInstance>.Current);
+    }
+
+    [Fact]
+    public void TestScopeToString()
+    {
+        var instance = new TestInstance();
+        using (var scope = new Scope<string, TestInstance>(instance))
+        {
+            Assert.Equal($"Scoping: {instance}", scope.ToString());
+        }
+    }
+
+    private class TestInstance
+    {
+        public override string ToString()
+        {
+            return "TestInstance";
+        }
+    }
+}
