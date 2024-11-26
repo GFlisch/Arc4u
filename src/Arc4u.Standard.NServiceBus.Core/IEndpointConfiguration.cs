06c292b66d3a8ccfc0c2e85b67396d478b76aa8b
Gilles Flisch <info@arc4u.net>
2024-11-26 10:32:37 +0100

    NServiceBus Core

--- src/Arc4u.Standard.NServiceBus.Core/IEndpointConfiguration.cs
+++ src/Arc4u.Standard.NServiceBus.Core/IEndpointConfiguration.cs
@@ -1,12 +1,10 @@
-﻿namespace Arc4u.NServiceBus
-{
-    /// <summary>
-    /// Define the contract to manage the configuration of a NServiceBus endpoint listener.
-    /// </summary>
-    public interface IEndpointConfiguration
-    {
-        // Start the listening on a queue.
-        Task StartAsync();
+namespace Arc4u.NServiceBus;
 
-    }
+/// <summary>
+/// Define the contract to manage the configuration of a NServiceBus endpoint listener.
+/// </summary>
+public interface IEndpointConfiguration
+{
+    // Start the listening on a queue.
+    Task StartAsync();
 }
--- src/Arc4u.Standard.NServiceBus.Core/MessagesToPublish.cs
+++ src/Arc4u.Standard.NServiceBus.Core/MessagesToPublish.cs
@@ -1,50 +1,46 @@
-﻿using Arc4u.Dependency.Attribute;
+using Arc4u.Dependency.Attribute;
 
-namespace Arc4u.NServiceBus
+namespace Arc4u.NServiceBus;
+
+[Export, Scoped]
+public class MessagesToPublish
 {
-    [Export, Scoped]
-    public class MessagesToPublish
+    private readonly List<Object> messages = new List<Object>();
+
+    public static Func<Type, bool> EventsNamingConvention = _ => false;
+
+    public static Func<Type, bool> CommandsNamingConvention = _ => false;
+
+    /// <summary>
+    /// Register a <see cref="ICommand"/> or <see cref="IEvent"/> to publish.
+    /// </summary>
+    /// <param name="message">Add a Command or event</param>
+    public void Add(object message)
     {
-        private readonly List<Object> messages = new List<Object>();
-
-        public static Func<Type, bool> EventsNamingConvention;
-
-        public static Func<Type, bool> CommandsNamingConvention;
-
-        /// <summary>
-        /// Register a <see cref="ICommand"/> or <see cref="IEvent"/> to publish.
-        /// </summary>
-        /// <param name="message">Add a Command or event</param>
-        public void Add(Object message)
+        if (null == EventsNamingConvention || null == CommandsNamingConvention)
         {
-            if (null == EventsNamingConvention || null == CommandsNamingConvention)
-            {
-                throw new AppException("No conventions is defined for Commands or Events.");
-            }
-
-            if (EventsNamingConvention(message.GetType()) || CommandsNamingConvention(message.GetType()))
-            {
-                messages.Add(message);
-            }
-            else
-            {
-                throw new AppException("Doesn't respect the namespace convention defined for events and commands.");
-            }
+            throw new AppException("No conventions is defined for Commands or Events.");
         }
 
-        /// <summary>
-        /// Used to clear any messages already registered.
-        /// </summary>
-        public void Clear()
+        if (EventsNamingConvention(message.GetType()) || CommandsNamingConvention(message.GetType()))
         {
-            if (null != messages)
-            {
-                messages.Clear();
-            }
+            messages.Add(message);
         }
-
-        public List<Object> Events => messages.Where((m) => EventsNamingConvention(m.GetType())).ToList();
-
-        public List<Object> Commands => messages.Where((m) => CommandsNamingConvention(m.GetType())).ToList();
+        else
+        {
+            throw new AppException("Doesn't respect the namespace convention defined for events and commands.");
+        }
     }
+
+    /// <summary>
+    /// Used to clear any messages already registered.
+    /// </summary>
+    public void Clear()
+    {
+        messages?.Clear();
+    }
+
+    public List<Object> Events => messages.Where((m) => EventsNamingConvention(m.GetType())).ToList();
+
+    public List<Object> Commands => messages.Where((m) => CommandsNamingConvention(m.GetType())).ToList();
 }
