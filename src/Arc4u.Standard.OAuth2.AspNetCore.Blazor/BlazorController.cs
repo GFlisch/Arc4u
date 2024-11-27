f6f7af58b41f358467688b78a86f2dd140523a8b
Gilles Flisch <info@arc4u.net>
2024-11-27 22:50:31 +0100

    Blazor

--- src/Arc4u.Standard.OAuth2.AspNetCore.Blazor/BlazorController.cs
+++ src/Arc4u.Standard.OAuth2.AspNetCore.Blazor/BlazorController.cs
@@ -1,7 +1,6 @@
-﻿using System.Net;
+using System.Net;
 using System.Security.Claims;
 using Arc4u.Dependency;
-using Arc4u.OAuth2.Aspect;
 using Arc4u.OAuth2.Token;
 using Arc4u.Security.Principal;
 using Arc4u.ServiceModel;
@@ -11,84 +10,85 @@ using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Arc4u.Blazor
-{
-    /// <summary>
-    ///  OboController class.
-    /// </summary>
-    [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    [ApiExplorerSettings(IgnoreApi = true)]
-    [ProducesResponseType(typeof(IEnumerable<Message>), StatusCodes.Status400BadRequest)]
-    public class BlazorController : ControllerBase
-    {
-        private const int buffer = 1024;
+namespace Arc4u.Blazor;
 
-        public BlazorController(ILogger<BlazorController> logger)
+/// <summary>
+///  OboController class.
+/// </summary>
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+[ApiExplorerSettings(IgnoreApi = true)]
+[ProducesResponseType(typeof(IEnumerable<Message>), StatusCodes.Status400BadRequest)]
+public class BlazorController : ControllerBase
+{
+    private const int buffer = 1024;
+
+    public BlazorController(ILogger<BlazorController> logger)
+    {
+        _logger = logger;
+    }
+
+    private readonly ILogger<BlazorController> _logger;
+
+    /// <summary>
+    /// This document will be part of the Sdk!
+    /// </summary>
+    /// <response code="2buffer">The Obo Bearer token</response>
+    /// <returns>The Obo name.</returns>
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
+    [HttpGet("redirectTo/{redirectTo}/{id?}")]
+    public async Task<IActionResult> Get(int? id, string redirectTo, [FromServices] IApplicationContext applicationContext, [FromServices] IContainerResolve containerResolve, [FromServices] ILogger<BlazorController> logger)
+    {
+        if (applicationContext.Principal is null || applicationContext.Principal.Authorization.Operations.Count == 0)
         {
-            _logger = logger;
+            return Unauthorized();
         }
 
-        private readonly ILogger<BlazorController> _logger;
+        string? accessToken = null;
 
-        /// <summary>
-        /// This document will be part of the Sdk!
-        /// </summary>
-        /// <response code="2buffer">The Obo Bearer token</response>
-        /// <returns>The Obo name.</returns>
-        [ServiceAspect()]
-        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
-        [HttpGet("redirectTo/{redirectTo}/{id?}")]
-        public async Task<IActionResult> Get(int? id, string redirectTo, [FromServices] IApplicationContext applicationContext, [FromServices] IContainerResolve containerResolve, [FromServices] ILogger<BlazorController> logger)
+        var index = id.HasValue ? id.Value! : 1;
+
+        if (applicationContext.Principal.Identity is ClaimsIdentity claimsIdentity)
         {
-            if (applicationContext.Principal.Authorization.Operations.Count == 0)
+            if (null != claimsIdentity.BootstrapContext)
             {
-                return Unauthorized();
-            }
-
-            string accessToken = null;
-
-            int index = id.HasValue ? id.Value : 1;
-
-            if (applicationContext.Principal.Identity is ClaimsIdentity claimsIdentity)
-            {
-                if (null != claimsIdentity.BootstrapContext)
-                {
-                    accessToken = claimsIdentity.BootstrapContext.ToString();
-                }
-                else
-                {
-                    if (containerResolve.TryResolve<IKeyValueSettings>("OpenId", out var settings))
-                    {
-                        if (containerResolve.TryResolve<ITokenProvider>(settings.Values[TokenKeys.ProviderIdKey], out var tokenProvider))
-                        {
-                            accessToken = (await tokenProvider.GetTokenAsync(settings, claimsIdentity)).Token;
-                        }
-                    }
-                }
-            }
-
-            if (string.IsNullOrEmpty(accessToken))
-            {
-                return BadRequest();
-            }
-
-            var redirectUrl = WebUtility.UrlDecode(redirectTo);
-
-            var redirectUri = "https://" + redirectUrl.TrimEnd('/') + "/_content/Arc4u.Standard.OAuth2.Blazor/GetToken.html";
-
-            if (accessToken.Length > index * buffer)
-            {
-                containerResolve.TryResolve<IKeyValueSettings>("OAuth2", out var settings);
-                var thisController = settings.Values[TokenKeys.RootServiceUrlKey].TrimEnd('/') + $"/blazor/redirectto/{redirectTo}/{index + 1}&token={accessToken.Substring((index - 1) * buffer, buffer)}";
-                return Redirect(UriHelper.Encode(new Uri($"{redirectUri}?url={thisController}")));
+                accessToken = claimsIdentity.BootstrapContext.ToString();
             }
             else
             {
-                return Redirect($"{redirectUri}?token={accessToken.Substring((index - 1) * buffer, accessToken.Length - (index - 1) * buffer)}");
+                if (containerResolve.TryResolve<IKeyValueSettings>("OpenId", out var settings))
+                {
+                    if (containerResolve.TryResolve<ITokenProvider>(settings!.Values[TokenKeys.ProviderIdKey], out var tokenProvider))
+                    {
+                        accessToken = (await tokenProvider!.GetTokenAsync(settings, claimsIdentity).ConfigureAwait(false)).Token;
+                    }
+                }
             }
+        }
+
+        if (string.IsNullOrEmpty(accessToken))
+        {
+            return BadRequest();
+        }
+
+        var redirectUrl = WebUtility.UrlDecode(redirectTo);
+
+        var redirectUri = "https://" + redirectUrl.TrimEnd('/') + "/_content/Arc4u.Standard.OAuth2.Blazor/GetToken.html";
+
+        if (accessToken.Length > index * buffer)
+        {
+            if (containerResolve.TryResolve<IKeyValueSettings>("OAuth2", out var settings))
+            {
+                var thisController = settings!.Values[TokenKeys.RootServiceUrlKey].TrimEnd('/') + $"/blazor/redirectto/{redirectTo}/{index + 1}&token={accessToken.Substring((index - 1) * buffer, buffer)}";
+                return Redirect(UriHelper.Encode(new Uri($"{redirectUri}?url={thisController}")));
+            }
+            return Unauthorized();
+        }
+        else
+        {
+            return Redirect($"{redirectUri}?token={accessToken.Substring((index - 1) * buffer, accessToken.Length - (index - 1) * buffer)}");
         }
     }
 }
--- src/Arc4u.Standard.OAuth2.Blazor/TokenProvider/BlazorMsalTokenProvider.cs
+++ src/Arc4u.Standard.OAuth2.Blazor/TokenProvider/BlazorMsalTokenProvider.cs
@@ -5,81 +5,81 @@ using Arc4u.OAuth2.Token;
 using Arc4u.Security.Principal;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication.Internal;
 
-namespace Arc4u.OAuth2.TokenProvider
+namespace Arc4u.OAuth2.TokenProvider;
+
+/// <summary>
+/// Provides an implementation of <see cref="ITokenProvider"/> for Blazor applications using MSAL.
+/// </summary>
+[Export(ProviderName, typeof(ITokenProvider))]
+public class BlazorMsalTokenProvider : ITokenProvider
 {
+    public const string ProviderName = "blazor";
+
     /// <summary>
-    /// Provides an implementation of <see cref="ITokenProvider"/> for Blazor applications using MSAL.
+    /// Initializes a new instance of the <see cref="BlazorMsalTokenProvider"/> class with the specified access token provider accessor and application context.
     /// </summary>
-    [Export(ProviderName, typeof(ITokenProvider))]
-    public class BlazorMsalTokenProvider : ITokenProvider
+    /// <param name="accessor">The access token provider accessor to use for obtaining access tokens.</param>
+    /// <param name="applicationContext">The application context to use for getting the principal.</param>
+    public BlazorMsalTokenProvider(IAccessTokenProviderAccessor accessor, IApplicationContext applicationContext)
     {
-        public const string ProviderName = "blazor";
+        _accessor = accessor;
+        _applicationContext = applicationContext;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BlazorMsalTokenProvider"/> class with the specified access token provider accessor and application context.
-        /// </summary>
-        /// <param name="accessor">The access token provider accessor to use for obtaining access tokens.</param>
-        /// <param name="applicationContext">The application context to use for getting the principal.</param>
-        public BlazorMsalTokenProvider(IAccessTokenProviderAccessor accessor, IApplicationContext applicationContext)
+    private readonly IAccessTokenProviderAccessor _accessor;
+    private readonly IApplicationContext _applicationContext;
+
+    /// <summary>
+    /// Asynchronously retrieves an OAuth2 token.
+    /// </summary>
+    /// <param name="settings">The settings to be used for getting the token.</param>
+    /// <param name="platformParameters">The platform-specific parameters, if any (not used in this implementation).</param>
+    /// <returns>A task representing the asynchronous operation, containing the requested <see cref="TokenInfo"/> if successful.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the settings parameter is null.</exception>
+    public async Task<TokenInfo> GetTokenAsync(IKeyValueSettings? settings, object? platformParameters)
+    {
+        ArgumentNullException.ThrowIfNull(settings);
+        ArgumentNullException.ThrowIfNull(_applicationContext.Principal);
+
+        if (null != _applicationContext.Principal)
         {
-            _accessor = accessor;
-            _applicationContext = applicationContext;
-        }
+            var identity = (ClaimsIdentity)_applicationContext.Principal.Identity!;
 
-        private readonly IAccessTokenProviderAccessor _accessor;
-        private readonly IApplicationContext _applicationContext;
-
-        /// <summary>
-        /// Asynchronously retrieves an OAuth2 token.
-        /// </summary>
-        /// <param name="settings">The settings to be used for getting the token.</param>
-        /// <param name="platformParameters">The platform-specific parameters, if any (not used in this implementation).</param>
-        /// <returns>A task representing the asynchronous operation, containing the requested <see cref="TokenInfo"/> if successful.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when the settings parameter is null.</exception>
-        public async Task<TokenInfo> GetTokenAsync(IKeyValueSettings settings, object platformParameters)
-        {
-            ArgumentNullException.ThrowIfNull(settings);
-
-            if (null != _applicationContext.Principal)
+            if (!string.IsNullOrWhiteSpace(identity.BootstrapContext?.ToString()))
             {
-                var identity = (ClaimsIdentity)_applicationContext.Principal.Identity;
-
-                if (!string.IsNullOrWhiteSpace(identity.BootstrapContext.ToString()))
-                {
-                    var token = identity.BootstrapContext.ToString();
-
-                    JwtSecurityToken jwt = new(token);
-
-                    if (jwt.ValidTo > DateTime.UtcNow)
-                    {
-                        return new TokenInfo("Bearer", token, jwt.ValidTo);
-                    }
-                }
-
-            }
-
-            var requestAccessToken = await _accessor.TokenProvider.RequestAccessToken();
-
-            if (requestAccessToken.TryGetToken(out var accessToken))
-            {
-                var token = accessToken.Value;
+                var token = identity.BootstrapContext.ToString();
 
                 JwtSecurityToken jwt = new(token);
-                return new TokenInfo("Bearer", token, jwt.ValidTo);
+
+                if (jwt.ValidTo > DateTime.UtcNow)
+                {
+                    return new TokenInfo("Bearer", token, jwt.ValidTo);
+                }
             }
 
-            return null;
         }
 
-        /// <summary>
-        /// Signs out the user by clearing the token.
-        /// </summary>
-        /// <param name="settings">The settings to be used for signing out (not used in this implementation).</param>
-        /// <param name="cancellationToken">The Cancellation token <see cref="CancellationToken"/></param>
-        /// <returns><see cref="ValueTask"/></returns>
-        public ValueTask SignOutAsync(IKeyValueSettings settings, CancellationToken cancellationToken)
+        var requestAccessToken = await _accessor.TokenProvider.RequestAccessToken().ConfigureAwait(false);
+
+        if (requestAccessToken.TryGetToken(out var accessToken))
         {
-            return ValueTask.CompletedTask;
+            var token = accessToken.Value;
+
+            JwtSecurityToken jwt = new(token);
+            return new TokenInfo("Bearer", token, jwt.ValidTo);
         }
+
+        throw new InvalidOperationException("No token found!");
+    }
+
+    /// <summary>
+    /// Signs out the user by clearing the token.
+    /// </summary>
+    /// <param name="settings">The settings to be used for signing out (not used in this implementation).</param>
+    /// <param name="cancellationToken">The Cancellation token <see cref="CancellationToken"/></param>
+    /// <returns><see cref="ValueTask"/></returns>
+    public ValueTask SignOutAsync(IKeyValueSettings settings, CancellationToken cancellationToken)
+    {
+        return ValueTask.CompletedTask;
     }
 }
