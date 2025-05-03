105c311c3fb201a6b19a576fb3e5f51f8dca8041
Gilles Flisch <info@arc4u.net>
2025-05-03 10:23:18 +0200

    Change the validation message and use the Severity to group them.

--- src/Arc4u.AspNetCore.Results/FromResultToProblemDetailExtension.cs
+++ src/Arc4u.AspNetCore.Results/FromResultToProblemDetailExtension.cs
@@ -25,10 +25,9 @@ public static class FromResultToProblemDetailExtension
         {
             //TODO: what to do when Code is null.
             var orderedErrors = errors.OfType<ValidationError>()
-                                      .OrderBy(x => x.Code)
-                                      .ThenBy(x => x.Severity)
-                                      .GroupBy(x => x.Code)
-                                      .ToImmutableSortedDictionary(g => g.Key, g => g.ToImmutableList().Select(vError => $"{vError.Severity}: {vError.Message}").ToArray());
+                                      .OrderBy(x => x.Severity)
+                                      .GroupBy(x => x.Severity)
+                                      .ToImmutableSortedDictionary(g => g.Key, g => g.ToImmutableList().Select(vError => vError.Message).ToArray());
 
             return new ValidationProblemDetails(orderedErrors)
                         .WithTitle("Error from validation.")
