379132882c7814f1e8d20f0b6adc370edf49b747
Arc4u <info@arc4u.net>
2025-11-03 23:36:19 +0100

    Add NullLoggerWrapper to ease unit testing and avoid Bad Arc4u usage.

---
+++ src/Arc4u.Diagnostics/Logging/NullLoggerWrapper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Arc4u.Diagnostics;
+
+/// <summary>
+/// Helper class to unit test the Arc4u logging.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class NullLoggerWrapper<T> : ILoggerWrapper<T>
+{
+    public static readonly NullLoggerWrapper<T> Instance = new();
+
+    private readonly ILogger<T> _logger = NullLogger<T>.Instance;
+
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        _logger.Log(logLevel, eventId, state, exception, formatter);
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return _logger.IsEnabled(logLevel);
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return _logger.BeginScope(state);
+    }
+
+    public void CallerMemberName(string caller)
+    {
+        // No implementation.
+    }
+
+    public Dictionary<string, object?> AdditionalFields { get; } = new();
+    public bool IncludeStackTrace { get; set; }
+    public ILoggerWrapper<T> SetContext(string category, string caller = "", Type? realType = null)
+    {
+        return this;
+    }
+}
---
+++ src/Arc4u.UnitTest/Logging/NullLoggerRapperTests.cs
@@ -0,0 +1,42 @@
+﻿using Arc4u.Diagnostics;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Arc4u.UnitTest.Logging;
+
+[Trait("Category", "CI")]
+public class NullLoggerRapperTests
+{
+    public NullLoggerRapperTests()
+    {
+        _fixture = new Fixture();
+        _fixture.Customize(new AutoMoqCustomization());
+    }
+    private readonly Fixture _fixture;
+
+    [Fact]
+    public void NullLoggetWrapper_Shoud()
+    {
+        var fixture = new Fixture();
+        fixture.Customize(new AutoMoqCustomization());
+
+
+        var loggerWrapper = NullLoggerWrapper<CategorySerilogTesters>.Instance;
+        fixture.Inject<ILogger<CategorySerilogTesters>>(loggerWrapper);
+
+        var logger = fixture.Create<ILogger<CategorySerilogTesters>>();
+        Assert.NotNull(logger.Technical());
+        Assert.NotNull(logger.Business());
+        Assert.NotNull(logger.Monitoring());
+        logger.Technical().LogDebug("Test");
+        logger.Technical().LogInformation("Test");
+        logger.Technical().LogWarning("Test");
+        logger.Technical().LogError("Test");
+        logger.Technical().LogCritical("Test");
+
+        logger.Technical().Add("key", "value").LogTrace("Test");
+
+    }
+}
